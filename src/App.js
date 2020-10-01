a141a1a9229b38a0977890558595b819c8ddb19a
ghadaali_1 <gali@uplandsoftware.com>
2020-10-01 16:43:13 +0200

    Adding React Query & Creating Components and Implementing Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import People from './components/People';
+import Planets from './components/Planets';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [page, setPage] = useState('planets');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h2 className="title">Stars War Info</h2>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </header>
     </div>
   );
