8a5d40a0f8e5c1350e9205812bc5873844c0a6d2
ghadaali_1 <gali@uplandsoftware.com>
2020-10-15 23:57:51 +0200

    Updates

---
+++ precache-manifest.090137d42a619f313ef3f1ecc6dc34ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281f3c17b412e4151c830a4c42c6a331",
+    "url": "/stars-war-react/index.html"
+  },
+  {
+    "revision": "42a26c25edb00140e2e3",
+    "url": "/stars-war-react/static/css/main.d99f4bb4.chunk.css"
+  },
+  {
+    "revision": "463965368e55d4aaa6b5",
+    "url": "/stars-war-react/static/js/2.01a99de5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stars-war-react/static/js/2.01a99de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a26c25edb00140e2e3",
+    "url": "/stars-war-react/static/js/main.a60aec0d.chunk.js"
+  },
+  {
+    "revision": "10506e3c3eb8833b0b9e",
+    "url": "/stars-war-react/static/js/runtime-main.c44165a3.js"
+  }
+]);
